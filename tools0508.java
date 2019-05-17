38357cad5a013991c7d6a3c8b56eb857b5f5c1cd
Regina 刘幸瑶 <48340679+reginaAaaaa@users.noreply.github.com>
2019-05-17 10:35:56 +0800

    Add files via upload

---
+++ tools0508.java
@@ -0,0 +1,19 @@
+public class tools0508{
+    //client program, tools.java
+    //main method
+    public static void main(String []args){
+    int a = 5;
+    int b = 10;
+    int c = tools.add(a, b);//parameter
+    System.out.println(c);
+    // 10 --> 5
+    int A = 10;
+   A =  tools.modify(10);
+    System.out.println(A);
+    //weekday day 7-->Sunday
+    }
+    
+
+}
+    
+    
